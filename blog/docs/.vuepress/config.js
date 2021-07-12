256caaaa0015509b4f764e3c46fab62db885f8f6
ggdream <gdream@yeah.net>
2021-07-12 20:43:36 +0800

    准备开始做作

---
+++ blog/docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    title: '魔咔啦咔的博博客',
+    description: '一时间不知道何去何从',
+    base: '/x/',
+    themeConfig: {
+        nav: [
+            {
+                text: '开源专区',
+                // link: '/open/',
+                items: [
+                    {
+                        text: 'OpenIM',
+                        link: '/open/openim/',
+                    },
+                    {
+                        text: 'Deve',
+                        link: '/open/deve/',
+                    },
+                    {
+                        text: 'SoftDB',
+                        link: '/open/softdb/',
+                    },
+                ]
+            },
+            {
+                text: '创作专区',
+                link: '/create/',
+            },
+        ],
+        sidebar: 'auto',
+    },
+    plugins: [
+        ['@vuepress/back-to-top'],
+        ['@vuepress/nprogress'],
+        '@vuepress/pwa',
+        {
+            serviceWorker: true,
+            updatePopup: true,
+        },
+    ],
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ],
+    // markdown: {
+    //     lineNumbers: true
+    // },
+}
---
+++ blog/docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroImage: http://qvgbcgfc6.hn-bkt.clouddn.com/1000.jpg
+actionText: 呜呜，我要回家
+actionLink: /
+features:
+- title: day day up
+  details: 记录每一天的进步, 一分耕耘，一分收获.
+- title: koala
+  details: 一个有趣的且乐于分享的人。座右铭：今天未完成的，明天更不会完成。
+footer: © 2021 魔咔啦咔 版权所有
+
+---
