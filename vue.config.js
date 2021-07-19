b5821e3a2e965f0213afecd3cfa041fdcedf33a0
ggdream <gdream@yeah.net>
2021-07-19 16:18:07 +0800

    使用terser混淆功能

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ],
-  chainWebpack: config => {
-      config.plugin('html').tap(args => {
-          args[0].title = '【魔咔啦咔】茕茕孑立，生生不息'
-          return args
-      })
-  },
-  productionSourceMap: false,
+    productionSourceMap: false,
+    transpileDependencies: [
+        'vuetify'
+    ],
+    chainWebpack: config => {
+        config.plugin('html').tap(args => {
+            args[0].title = '【魔咔啦咔】茕茕孑立，生生不息'
+            return args
+        })
+        config.when(process.env.NODE_ENV === 'production', config => {
+            config.optimization.minimizer('terser').tap((args) => {
+                args[0].terserOptions.compress.drop_console = true
+                return args
+            })
+        })
+    },
 }
